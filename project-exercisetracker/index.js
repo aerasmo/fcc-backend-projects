6ebeb25669ee714bc0a23d7a92b65c8aa3bc0706
aerasmo <angelo1erasmo@gmail.com>
2022-07-18 11:16:09 +0800

    Refactor code

--- project-exercisetracker/index.js
+++ project-exercisetracker/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
+
 const cors = require('cors')
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
+
 const User = require("./models/Users")
 const Exercise = require('./models/Exercise')
-const formatDate = require('./utils')
 
 require('dotenv').config()
 
+// Setup middlewares
 app.use(express.static('public'))
 
 app.use(cors());
@@ -27,21 +29,31 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true }
 )
 
+// Index route
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 // User routes
-app.post('/api/users', async (req, res) => {
+app.route('/api/users').get(async(req, res) => {
+  try {
+    let users = await User.find({}).select("username _id");
+    console.log(users)
+    res.send(users)
+  } catch(e) {
+    console.log(e.message);
+    res.redirect("/")
+  }
+}).post(async (req, res) => {
   let { username } = req.body;
-  console.log(req.body)
   try {
     let user = await User.create( { username: username })
+    console.log(user)
     user =  { 
       username: user.username, 
       _id:  user._id
     }
-    console.log(user)
+    
     res.json(user)
   } catch(e) {
     console.log(e.message)
@@ -49,24 +61,16 @@ app.post('/api/users', async (req, res) => {
   }
 });
 
-app.get('/api/users', async(req, res) => {
-  try {
-    let users = await User.find({}).select("username _id");
-    res.send(users)
-  } catch(e) {
-    console.log(e.message);
-    res.redirect("/")
-  }
-})
-
 // Exercise routes
 app.post('/api/users/:_id/exercises', async(req, res) => {
   let _id = req.body[":_id"] || req.params._id;
   let { description, duration, date} = req.body;
-  let json_response;
+  let jsonResponse;
+
   if (!date) {
     date = new Date();
   }
+
   try {
     let user = await User.findById(_id); 
     let exercise = await Exercise.create({ description, duration, date, user: _id});
@@ -75,7 +79,7 @@ app.post('/api/users/:_id/exercises', async(req, res) => {
     user.log.push(exercise._id);
     user.save()
 
-    json_response = {
+    jsonResponse = {
       "username": user.username,
       "_id": _id,
       "description": exercise.description,
@@ -84,11 +88,11 @@ app.post('/api/users/:_id/exercises', async(req, res) => {
     } 
   } catch(e) {
     console.log(e.message)
-    json_response = {
+    jsonResponse = {
       "error": e.message
     }
   }
-  res.json(json_response)
+  res.json(jsonResponse)
 
 })
 
@@ -96,7 +100,8 @@ app.post('/api/users/:_id/exercises', async(req, res) => {
 app.get('/api/users/:_id/logs', async(req, res) => {
   let { _id } = req.params;
   let { from , to, limit } = req.query;
-  let json_response;
+  let jsonResponse;
+
   try {
     let user = await User.findById(_id);
     let exercise, formattedDate;
@@ -111,19 +116,22 @@ app.get('/api/users/:_id/logs', async(req, res) => {
     if (from || to) {
       query["date"] = dateQuery;
     }
-
     if (limit) {
       exercise = await Exercise.find(query, {_id: 0, description: 1, duration:1, date: 1}).limit(limit);
     } else {
       exercise = await Exercise.find(query, {_id: 0, description: 1, duration:1, date: 1});
     }
+
     exercise = exercise.map( obj => {
       formattedDate = obj.date.toDateString();
       let {description, duration} = obj
       return {description, duration, date: formattedDate}
     })
 
-    json_response = {
+    console.log(user)
+    console.log(exercise)
+
+    jsonResponse = {
       username: user.username,
       count: exercise.length,
       _id: user.id,
@@ -131,13 +139,13 @@ app.get('/api/users/:_id/logs', async(req, res) => {
     };
   } catch(e) {
     console.log(e.message);
-    json_response = {
+    jsonResponse = {
       error: e.message
     }
   }
-
-  res.json(json_response);
+  res.json(jsonResponse);
 })
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
--- project-exercisetracker/utils.js
+++
@@ -1,7 +0,0 @@
-const formatDate = (date) => {
-    // => Mon Jan 01 1990
-    var options = { weekday: "short", month: 'short', year: 'numeric', day: 'numeric'}
-    return date.toLocaleDateString("en-US", options).replaceAll(",", "")
-}
-
-module.exports = formatDate
